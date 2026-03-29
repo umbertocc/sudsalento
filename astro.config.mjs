5885f9e357544e8b4ff9d73db04b68fc116ad9d8
umbertocc <68420634+umbertocc@users.noreply.github.com>
2026-03-29 14:13:31 +0200

    Update Astro config for site and base settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
+  // 1. Il dominio principale (senza slash finale)
+  site: 'https://umbertocc.github.io',
+  
+  // 2. La sottocartella del repository (con gli slash)
+  base: '/sudsalento',
+
   vite: {
     plugins: [tailwindcss()]
   }
