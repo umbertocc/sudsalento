5427e292a906638c5dfe130e480464f5c0b272c2
Umberto <xesi09@informatica.aci.it>
2026-03-29 14:14:39 +0200

    feat

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
+  outDir: './docs',
   vite: {
     plugins: [tailwindcss()]
   }
